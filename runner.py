4d28a831883e91f8c43bb59ebac4bcac28b8c6a1
Dench991228 <denghanzhi85139@163.com>
2023-02-10 12:01:07 +0800

    稍微调整了逻辑

--- runner.py
+++ runner.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         os.mkdir("./logs")
     # 设置日志相关内容
     lg = logging.getLogger(f"runner")
-    logging.basicConfig(filename=f"./logs/runner.txt", filemode='a', level=logging.DEBUG,
+    logging.basicConfig(filename=f"./logs/runner.txt", filemode='a', level=logging.INFO,
                         format="%(levelname)s %(asctime)s %(funcName)s %(lineno)d %(message)s")
     # 获取redis客户端
     client = redis.Redis(host=os.environ.get("MYSERVER"), port=6379, password=os.environ.get("REDISCLI_AUTH"))
@@ -33,10 +33,10 @@ if __name__ == '__main__':
                 f.close()
             try:
                 try_tweet_by_id_scrap(task['id'], task['save_dir'])
-                lg.log(logging.DEBUG, f"Successfully scraped {task['id']} in day {task['date_str']}, {count} tweets remaining")
+                lg.log(logging.INFO, f"Successfully scraped {task['id']} in day {task['date_str']}, {count} tweets remaining")
             except Exception as ex:
-                lg.log(logging.WARN, f"An error has took place{ex}")
+                lg.log(logging.WARN, f"An error has took place{ex}, when scraping {task['id']}")
                 client.rpush("id_scrape_tasks", json.dumps(task))
                 count += 1
                 client.set(task['date_str'], count)
-                time.sleep(60)
+                time.sleep(5)
